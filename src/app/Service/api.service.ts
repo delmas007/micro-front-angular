4d6ad1e825c3fe077e180c3363ac79dc3924fb93
delmas007 <azertyazerty1ze@gmail.com>
2024-07-04 13:00:30 +0000

    login

--- src/app/Service/api.service.ts
+++ src/app/Service/api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {firstValueFrom} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private host1 = 'http://localhost:8000/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  async Login(username: string, password: string): Promise<any> {
+    const body = { username: username, password: password };
+    return await firstValueFrom(
+      this.http.post<any>(`${this.host1}/login`, body)
+    );
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {Router, RouterLink} from "@angular/router";
+import {NgIf} from "@angular/common";
+import {ApiService} from "../Service/api.service";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    NgIf
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  formLogin!: FormGroup;
+  errorMessage: any;
+  loading = false;
+
+  constructor(private fb:FormBuilder,private apiService: ApiService, private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    this.formLogin = this.fb.group({
+      // email: ['', [Validators.required, Validators.email]],
+      // password: ['', [Validators.required, Validators.minLength(6)]]
+      username: this.fb.control(""),
+      password: this.fb.control("")
+    });
+  }
+
+  handleLogin() {
+    let username = this.formLogin.value.username;
+    let password = this.formLogin.value.password;
+    this.loading = true;
+    this.errorMessage = null;
+    this.apiService.Login(username, password)
+      .then((token: any) => {
+        console.log(token);
+        // this.router.navigateByUrl(`/verification/${token.email}`)
+      })
+      .catch(err => {
+        if (err.error && err.error.message) {
+          this.errorMessage = err.error.message;
+        } else {
+          this.errorMessage = 'Mot de passe incorrecte';
+        }
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  }
+
 
 }
